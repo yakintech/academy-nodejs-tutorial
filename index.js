55c4218285e4dac03718f70435b5d0e2271bfd05
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-01-27 16:44:26 +0400

    cors policy

--- index.js
+++ index.js
@@ -18,14 +18,13 @@ app.use('/api/contact', contactRouter);
 app.use('/api/products', productRouter);
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      '*',
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept , Authorization"
     );
     next();
-  });
+});
 
 
 app.use(function (req, res, next) {
